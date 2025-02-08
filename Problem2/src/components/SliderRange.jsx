1c8c8c623edba9a44edfb01018930b4c63d02a26
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2025-02-08 11:24:30 +0530

    commit

--- Problem2/src/components/SliderRange.jsx
+++ Problem2/src/components/SliderRange.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import styles from "./RangeSlider.module.scss";
 
 const RangeSlider = () => {
+  const max = 100;
+  const min = 1;
   const [minValue, setMinValue] = useState(25);
   const [maxValue, setMaxValue] = useState(75);
 
@@ -10,7 +12,7 @@ const RangeSlider = () => {
   }, [minValue, maxValue]);
 
   const updateProgress = () => {
-    const range = 100 - 1;
+    const range = max - min;
     const valueRange = maxValue - minValue;
     const width = (valueRange / range) * 100;
     const minOffset = ((minValue - 1) / range) * 100;
@@ -18,49 +20,28 @@ const RangeSlider = () => {
     document.documentElement.style.setProperty("--progress-width", `${width}%`);
     document.documentElement.style.setProperty("--progress-left", `${minOffset}%`);
   };
-
   return (
-    <div className={styles.container}>
-      <div className={styles.rangeSlider}>
-        <input
-          type="number"
-          className={styles.minPrice}
-          value={minValue}
-          min="1"
-          max="100"
-          onChange={(e) => setMinValue(Math.min(+e.target.value, maxValue-1))}
-        />
-        <input
-          type="number"
-          className={styles.maxPrice}
-          value={maxValue}
-          min="1"
-          max="100"
-          onChange={(e) => setMaxValue(Math.max(+e.target.value, minValue+1))}
-        />
-        <div className={styles.range}>
-          <input
-            type="range"
-            className={styles.minInput}
-            value={minValue}
-            min="1"
-            max="100"
-            onChange={(e) => setMinValue(Math.min(+e.target.value, maxValue))}
-          />
-          <input
-            type="range"
-            className={styles.maxInput}
-            value={maxValue}
-            min="1"
-            max="100"
-            onChange={(e) => setMaxValue(Math.max(+e.target.value, minValue))}
-          />
-          <div className={styles.slider}>
-            <div className={styles.progress}></div>
-          </div>
-        </div>
-      </div>
+  <div className={styles.range}>
+    <input
+      type="range"
+      className={styles.minInput}
+      value={minValue}
+      min={min}
+      max={max}
+      onChange={(e) => setMinValue(Math.min(+e.target.value, maxValue))}
+    />
+    <input
+      type="range"
+      className={styles.maxInput}
+      value={maxValue}
+      min="1"
+      max="100"
+      onChange={(e) => setMaxValue(Math.max(+e.target.value, minValue))}
+    />
+    <div className={styles.slider}>
+      <div className={styles.progress}></div>
     </div>
+  </div>
   );
 };
 
